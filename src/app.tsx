aa66d58c1fe7f07790607a4182be0265be31b006
Luis Fernando <luisfernandobr2003@gmail.com>
2024-01-31 15:28:44 -0300

    feat: add theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { queryClient } from './lib/react-query'
 import { router } from './routes'
@@ -11,13 +12,15 @@ import { router } from './routes'
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
+      <ThemeProvider defaultTheme="dark" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
 
-      <Toaster richColors />
+        <Toaster richColors />
 
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
