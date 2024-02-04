a0623726f2c928d0f5b3afe6e0460a9f7ea4bfe8
Luiz1nn <luisfernandobr2003@gmail.com>
2024-02-04 14:31:03 -0300

    feat: add error component

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Error } from './pages/app/error'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sing-up'
 
@@ -10,6 +11,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
