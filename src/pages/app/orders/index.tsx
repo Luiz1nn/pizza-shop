688f75075bf57c656c3eb61d5793c6b62953ea9c
Luiz1nn <luisfernandobr2003@gmail.com>
2024-02-04 17:10:16 -0300

    feat(orders): add order table filter component

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -6,6 +6,7 @@ import { z } from 'zod'
 import { getOrders } from '~/api/get-orders'
 import { Table, TableHead, TableHeader, TableRow } from '~/components/ui/table'
 
+import { OrderTableFilter } from './order-table-filter'
 import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
@@ -38,6 +39,8 @@ export function Orders() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
+          <OrderTableFilter />
+
           <div className="rounded-md border">
             <Table>
               <TableHeader>
